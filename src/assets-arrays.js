c96aa9654a78221f899238d101baa6d190b60f7e
Sarah <sarahtvana@gmail.com>
2023-12-26 06:41:31 -0600

    gh pages syntax update

--- src/assets-arrays.js
+++ src/assets-arrays.js
@@ -1,112 +1,112 @@
 export const categories = [
     {
         id: "Backgrounds",
-        icon: "/assets/backgrounds/icon.png"
+        icon: "assets/backgrounds/icon.png"
     },
     {
         id: "Bases",
-        icon: "/assets/bases/icon.png"
+        icon: "assets/bases/icon.png"
     },
     {
         id: "Outfits",
-        icon: "/assets/outfits/icon.png"
+        icon: "assets/outfits/icon.png"
     }
 ]
 
 export const backgrounds = [
     { id: "lace",
         colors: [{
-            icon: "/assets/outfits/null-icon.png",
-            image: "/assets/outfits/empty.png"}
+            icon: "assets/outfits/null-icon.png",
+            image: "assets/outfits/empty.png"}
     ],
-      icon:"/assets/backgrounds/background.png",
-      image: '/assets/backgrounds/background.png'
+      icon:"assets/backgrounds/background.png",
+      image: 'assets/backgrounds/background.png'
     },
     { id: "bg2",
       colors: [{
-        icon: "/assets/outfits/null-icon.png",
-        image: "/assets/outfits/empty.png"}
+        icon: "assets/outfits/null-icon.png",
+        image: "assets/outfits/empty.png"}
     ],
-      icon:"/assets/backgrounds/bg2.png",
-      image: '/assets/backgrounds/bg2.png'
+      icon:"assets/backgrounds/bg2.png",
+      image: 'assets/backgrounds/bg2.png'
     },
     { id: "bg3",
       colors: [{
-        icon: "/assets/outfits/null-icon.png",
-        image: "/assets/outfits/empty.png"}
+        icon: "assets/outfits/null-icon.png",
+        image: "assets/outfits/empty.png"}
     ],
-      icon:"/assets/backgrounds/bg3.png",
-      image: '/assets/backgrounds/bg3.png'
+      icon:"assets/backgrounds/bg3.png",
+      image: 'assets/backgrounds/bg3.png'
     },
     { id: "bg4",
       colors: [{
-        icon: "/assets/outfits/null-icon.png",
-        image: "/assets/outfits/empty.png"}
+        icon: "assets/outfits/null-icon.png",
+        image: "assets/outfits/empty.png"}
     ],
-      icon:"/assets/backgrounds/bg4.png",
-      image: '/assets/backgrounds/bg4.png'
+      icon:"assets/backgrounds/bg4.png",
+      image: 'assets/backgrounds/bg4.png'
   }
    ]
 
 export const bases = [{
     id:"baseguy",
-    icon: "/assets/bases/icon.png",
+    icon: "assets/bases/icon.png",
     colors: [{
-        icon: "/assets/outfits/null-icon.png",
-        image: "/assets/outfits/empty.png"}
+        icon: "assets/outfits/null-icon.png",
+        image: "assets/outfits/empty.png"}
     ],
-    image: '/assets/bases/baseguy.png'}
+    image: 'assets/bases/baseguy.png'}
 ]
 
 export const outfits= [
     {
         id: "no outfit",
-        icon: "/assets/outfits/null-icon.png",
+        icon: "assets/outfits/null-icon.png",
         colors: [
             {
-            icon: "/assets/outfits/null-icon.png",
-            image: "/assets/outfits/empty.png"
+            icon: "assets/outfits/null-icon.png",
+            image: "assets/outfits/empty.png"
         }],
-        image: '/assets/outfits/empty.png'
+        image: 'assets/outfits/empty.png'
     },
     {
         id: "dress 1",
-        icon: "/assets/outfits/dress1/dress1.png",
+        icon: "assets/outfits/dress1/dress1.png",
         colors: [
             {
-                icon: "/assets/outfits/null-icon.png",
-                image: "/assets/outfits/empty.png"},
+                icon: "assets/outfits/null-icon.png",
+                image: "assets/outfits/empty.png"},
             {
-                icon: "/assets/outfits/dress1/dress1color-red-icon.png",
-                image: "/assets/outfits/dress1/dress1color.png"
+                icon: "assets/outfits/dress1/dress1color-red-icon.png",
+                image: "assets/outfits/dress1/dress1color.png"
              }],
-        image: '/assets/outfits/dress1/dress1.png'
+        image: 'assets/outfits/dress1/dress1.png'
     },
     {
         id: "dress 2",
-        icon: "/assets/outfits/dress2/dress2.png",
+        icon: "assets/outfits/dress2/dress2.png",
         colors: [{
-            icon: "/assets/outfits/null-icon.png",
-            image: "/assets/outfits/empty.png"
+            icon: "assets/outfits/null-icon.png",
+            image: "assets/outfits/empty.png"
         },
         {
-            icon: "/assets/outfits/dress2/dress2color-red-icon.png",
-            image: "/assets/outfits/dress2/dress2color-red.png"
+            icon: "assets/outfits/dress2/dress2color-red-icon.png",
+            image: "assets/outfits/dress2/dress2color-red.png"
         },
         {
-            icon: "/assets/outfits/dress2/dress2color-blue-icon.png",
-            image: "/assets/outfits/dress2/dress2color-blue.png"
+            icon: "assets/outfits/dress2/dress2color-blue-icon.png",
+            image: "assets/outfits/dress2/dress2color-blue.png"
         },
         {
-            icon: "/assets/outfits/dress2/dress2color-yellow-icon.png",
-            image: "/assets/outfits/dress2/dress2color-yellow.png"
+            icon: "assets/outfits/dress2/dress2color-yellow-icon.png",
+            image: "assets/outfits/dress2/dress2color-yellow.png"
         },
         {
-            icon: "/assets/outfits/dress2/dress2color-black-icon.png",
-            image: "/assets/outfits/dress2/dress2color-black.png"
+            icon: "assets/outfits/dress2/dress2color-black-icon.png",
+            image: "assets/outfits/dress2/dress2color-black.png"
         },
         ],
-        image: '/assets/outfits/dress2/dress2.png'
+        image: 'assets/outfits/dress2/dress2.png'
     }
 ]
 
